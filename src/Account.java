be9498f76ff1692a70aad46c0943fbdf09cb064b
Ali Uyanik <egeninefesi03@gmail.com>
2023-03-01 00:14:39 +0300

    v6

--- src/Account.java
+++ src/Account.java
@@ -1,7 +1,11 @@
 public class Account {
     //ali branchinde bu islem yapildi
     public String name;
-    //master branchinde degisiklik yaptim
+    //ali branchinde degisiklik yaptimisim
+    //sonra master bransimla merge yaptim
+    //sonra bunu da push yaptim.
+    //sonra simdi de bunu yapacagim
+
     
 
 }
